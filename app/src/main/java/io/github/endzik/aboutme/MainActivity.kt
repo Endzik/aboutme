4531bfd914f126aef68b42ff72fd307edcabaef3
Andrzej Grabowski <andrzej.grabowski@vimn.com>
2021-02-08 19:07:14 +0100

    Initial commit

---
+++ app/src/main/java/io/github/endzik/aboutme/MainActivity.kt
@@ -0,0 +1,40 @@
+package io.github.endzik.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import io.github.endzik.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Andrzej Goetzendorf-Grabowski")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener { addNickname(it) }
+    }
+
+    private fun addNickname(view: View) {
+
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ app/src/main/java/io/github/endzik/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package io.github.endzik.aboutme
+
+data class MyName (
+    var name: String = "",
+    var nickname: String = ""
+)
